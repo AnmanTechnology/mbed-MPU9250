b3024e5aab0bec2b0f129d40ab4fff503b6d2ba6
Liews Wuttipat <sweilz.w@gmail.com>
2019-11-29 21:36:12 +0700

    regist pio lib

---
+++ MPU9250.h
@@ -0,0 +1,23 @@
+#ifndef _MPU9250_H_
+#define _MPU9250_H_
+
+#include <mbed.h>
+
+class MPU9250
+{
+private:
+    /* data */
+public:
+    MPU9250(/* args */);
+    ~MPU9250();
+};
+
+MPU9250::MPU9250(/* args */)
+{
+}
+
+MPU9250::~MPU9250()
+{
+}
+
+#endif
